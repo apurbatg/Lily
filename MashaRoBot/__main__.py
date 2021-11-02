4e3b3230388f7fa3165c68fcf0fbe659ae1729da
apurbatg <93421564+apurbatg@users.noreply.github.com>
2021-11-02 15:36:38 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -75,7 +75,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Heya! My name is Asuna.
-I'm here to help you manage your groups! Hit *📚Commands* button below to find out more about how to use me to my full potential. 
+I'm here to help you manage your groups! Hit *😊Commands* button below to find out more about how to use me to my full potential. 
 """
 
 buttons = [
