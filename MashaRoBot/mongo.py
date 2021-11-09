1e2a1e80fbfa64363365b995a2a85a100a6ca37d
apurbatg <93421564+apurbatg@users.noreply.github.com>
2021-11-09 18:17:17 +0530

    Update mongo.py

--- MashaRoBot/mongo.py
+++ MashaRoBot/mongo.py
@@ -1,11 +1,13 @@
+#war-legend
 import asyncio
 import sys
 
+from MashaRoBot import log
 from motor import motor_asyncio
 from MashaRoBot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from TGNRobot.conf import get_int_key, get_str_key
+from MashaRoBot.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
@@ -17,7 +19,7 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["nezuko"]
+db = client["AmeliaRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
